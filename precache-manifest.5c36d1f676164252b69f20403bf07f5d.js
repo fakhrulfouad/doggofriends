9a47b02c1cf69bc876760bf835df3a60dab43925
Muhammad Fakhrulghazi bin Mohd Fouad <muhammadfakhrulghazibinmohdfouad@Muhammads-MacBook-Pro.local>
2020-09-07 12:40:30 +0800

    Updates

---
+++ precache-manifest.5c36d1f676164252b69f20403bf07f5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e7fc700ddad4b7b167d99c5bf88272",
+    "url": "/doggofriends/index.html"
+  },
+  {
+    "revision": "6e18ae11a2e280d832cc",
+    "url": "/doggofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c672935b78bdfd8a9382",
+    "url": "/doggofriends/static/css/main.9538fb17.chunk.css"
+  },
+  {
+    "revision": "6e18ae11a2e280d832cc",
+    "url": "/doggofriends/static/js/2.d78fbabe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doggofriends/static/js/2.d78fbabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c672935b78bdfd8a9382",
+    "url": "/doggofriends/static/js/main.6370351d.chunk.js"
+  },
+  {
+    "revision": "27c8d07a2bbaba796798",
+    "url": "/doggofriends/static/js/runtime-main.59fc6554.js"
+  },
+  {
+    "revision": "0c047336538351953f6029d6204cd2b0",
+    "url": "/doggofriends/static/media/Minnie.0c047336.woff"
+  },
+  {
+    "revision": "08857db1dae4f18c13b0ad922a2c6f91",
+    "url": "/doggofriends/static/media/wds052801.08857db1.woff"
+  }
+]);
